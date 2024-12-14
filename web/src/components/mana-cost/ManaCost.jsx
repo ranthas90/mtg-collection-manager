244d5eb3676f70a31135a96b29ef8117fec4b587
ruben <rubenreal1990@gmail.com>
2024-12-14 10:42:59 +0000

    Changed mana cost URL

--- web/src/components/mana-cost/ManaCost.jsx
+++ web/src/components/mana-cost/ManaCost.jsx
@@ -2,7 +2,7 @@ const ManaCost = ({ manaCost }) => {
     return (
         <div className="flex flex-row">
             {manaCost && manaCost.map((url, index) => (
-                <img key={index} src={url} className="max-w-none w-[16px] h-[16px]" alt="Mana cost" />
+                <img key={index} src={`/assets/symbols/${url}`} className="max-w-none w-[16px] h-[16px]" alt="Mana cost" />
             ))}
         </div>
     );
