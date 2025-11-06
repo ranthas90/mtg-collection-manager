c04e2bcc52994a4edbde2d22db6383e1698302b7
AdQ-RubenReal <ruben.real@adquiver.com>
2025-11-06 16:24:43 +0000

    wip

--- mtgcm-web/src/app/mtg-expansions/mtg-expansions-page.tsx
+++ mtgcm-web/src/app/mtg-expansions/mtg-expansions-page.tsx
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+import { useAxios } from "../../hooks/use-axios.ts";
+
 export function MtgExpansionsPage() {
+  const axios = useAxios();
+  const [sets, setSets] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get("/collection/sets");
+    };
+    fetchData();
+  }, []);
   return <div>My ExpansionsPage</div>;
 }
---
+++ mtgcm-web/src/hooks/use-axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export function useAxios() {
+  return axios.create({
+    baseURL: "http://localhost:8080",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  });
+}
