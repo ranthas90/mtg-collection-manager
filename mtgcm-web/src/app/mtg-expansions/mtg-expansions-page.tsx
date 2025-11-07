263d1950cd8516248218465670529424ad790f15
Rubén Real Morales <rubenreal1990@gmail.com>
2025-11-07 09:16:56 +0000

    lost changes

--- mtgcm-web/src/app/mtg-expansions/mtg-expansions-page.tsx
+++ mtgcm-web/src/app/mtg-expansions/mtg-expansions-page.tsx
@@ -1,3 +1,19 @@
 export function MtgExpansionsPage() {
-  return <div>My ExpansionsPage</div>;
+  return (
+    <>
+      <h1>Magic: The Gathering expansions</h1>
+      <h3>
+        Search a list of all Magic: The Gathering expansions, ordered by newest
+        to oldest.
+      </h3>
+      <div>Aquí irán los filtros (nombre, tipo expansion)</div>
+      <div>
+        Aquí irá la tabla con las expansiones donde cada columna es: 1 - icono +
+        nombre set + badge con el tipo de set 2 - porcentaje de coleccion
+        completada 3 - Valor total del set (solo normales) 4 - Valor total de
+        las cartas coleccionadas 5 - Código de la coleccion 6 - Fecha de
+        lanzamiento NOTA:
+      </div>
+    </>
+  );
 }
