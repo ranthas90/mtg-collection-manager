193d27de6672d5ee8fc9769a973680eb08de7ed0
AdQ-RubenReal <ruben.real@adquiver.com>
2026-07-28 12:44:21 +0100

    WIP - Added Cardmarket api client and fixes

--- mtgcm-api/src/main/java/org/ranthas/mtgcmapi/MtgcmApiApplication.java
+++ mtgcm-api/src/main/java/org/ranthas/mtgcmapi/MtgcmApiApplication.java
@@ -3,11 +3,20 @@ package org.ranthas.mtgcmapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class MtgcmApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(MtgcmApiApplication.class, args);
+		openHomePage();
 	}
 
+	// Abre el navegador directamente.
+	// TODO: esto fijo que solo funciona en Windows! Hay que probar el soporte para MacOS
+	private static void openHomePage() throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8081");
+	}
 }
